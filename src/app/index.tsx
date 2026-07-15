cb374249bfca046c6b9830ea6e558f3015626b97
Kailash Rawat <kailash2005rawat@gmail.com>
2026-07-15 11:57:44 +0530

    feat: initialize home screen component and add linting dependencies

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,2 +1,10 @@
-import {Text, View} from 'react-native'
+import { Text, View } from "react-native";
 
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
